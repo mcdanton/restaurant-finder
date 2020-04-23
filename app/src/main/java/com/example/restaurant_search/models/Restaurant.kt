7ff6bc6482ea47bbf8f73675aedda26f8e8d2058
mcdanton <danhefter@gmail.com>
2020-04-23 11:40:58 -0400

    Added Restaurant Model

---
+++ app/src/main/java/com/example/restaurant_search/models/Restaurant.kt
@@ -0,0 +1,10 @@
+package com.example.restaurant_search.models
+
+class Restaurant {
+
+    var name: String = ""
+    var address: String = ""
+    var priceRange: String = ""
+    var description: String = ""
+    
+}
